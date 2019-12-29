e193fb2f4690ed445c91d502a574c9609b5f1d70
hecl <598694302@qq.com>
2019-12-29 13:05:49 +0800

    注册中心配置及认证配置

---
+++ eureka-server-ha/src/test/java/com/myproject/eurekaserverha/EurekaServerHaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.eurekaserverha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerHaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
