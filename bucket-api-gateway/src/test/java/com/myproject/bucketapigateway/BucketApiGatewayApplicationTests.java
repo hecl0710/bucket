0fb18c8da384094430a99e202d030f12893bb73e
hecl <598694302@qq.com>
2020-01-04 16:27:39 +0800

    zuul配置

---
+++ bucket-api-gateway/src/test/java/com/myproject/bucketapigateway/BucketApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.bucketapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BucketApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
